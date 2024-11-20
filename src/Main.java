31fc9c09c317d52447ce71acf32cfcae821cdbac
pc <pc@HAMZA-LAPTOP>
2024-11-20 10:57:53 +0100

    Sum function

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi");
+		int sum=0;
+		for(int i=0;i<10;i++) {
+			sum+=i;
+		}
+		System.out.println("Sum = "+sum);
+		
 	}
 
 }
